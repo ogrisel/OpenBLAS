573a9bd8f89c1f6f6f26f73699f42a8582fc0531
Olivier Grisel <olivier.grisel@ensta.org>
2014-02-18 13:25:22 +0100

    Improve fork test by checking nested forks

--- utest/test_fork.c
+++ utest/test_fork.c
@@ -46,6 +46,17 @@ void* xmalloc(size_t n)
     }
 }
 
+void check_dgemm(double *a, double *b, double *result, double *expected, int n)
+{
+    int i;
+    cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n,
+        1.0, a, n, b, n, 0.0, result, n);
+    for(i = 0; i < n * n; ++i) {
+        CU_ASSERT_DOUBLE_EQUAL(expected[i], result[i], CHECK_EPS);
+    }
+}
+
+
 void test_fork_safety(void)
 {
     int n = 1000;
@@ -55,6 +66,7 @@ void test_fork_safety(void)
     size_t n_bytes;
      
     pid_t fork_pid;
+    pid_t fork_pid_nested;
 
     n_bytes = sizeof(*a) * n * n;
      
@@ -76,25 +88,31 @@ void test_fork_safety(void)
 
     fork_pid = fork();
     if (fork_pid == -1) {
-        CU_FAIL("Failed to fork process.")
+        CU_FAIL("Failed to fork process.");
     } else if (fork_pid == 0) {
         // Compute a DGEMM product in the child process to check that the
         // thread pool as been properly been reinitialized after the fork.
-        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n,
-            1.0, a, n, b, n, 0.0, d, n);
+        check_dgemm(a, b, d, c, n);
 
-        for(i = 0; i < n * n; ++i) {
-            CU_ASSERT_DOUBLE_EQUAL(c[i], d[i], CHECK_EPS);
+        // Nested fork to check that the pthread_atfork protection can work
+        // recursively
+        fork_pid_nested = fork();
+        if (fork_pid_nested == -1) {
+            CU_FAIL("Failed to fork process.");
+            exit(1);
+        } else if (fork_pid_nested == 0) {
+            check_dgemm(a, b, d, c, n);
+            exit(0);
+        } else {
+            check_dgemm(a, b, d, c, n);
+            int child_status = 0;
+            pid_t wait_pid = wait(&child_status);
+            CU_ASSERT(wait_pid == fork_pid_nested);
+            CU_ASSERT(WEXITSTATUS (child_status) == 0);
+            exit(0);
         }
-        exit(0);
     } else {
-        cblas_dgemm(CblasRowMajor, CblasNoTrans, CblasNoTrans, n, n, n,
-            1.0, a, n, b, n, 0.0, d, n);
-
-        for(i = 0; i < n * n; ++i) {
-            CU_ASSERT_DOUBLE_EQUAL(c[i], d[i], CHECK_EPS);
-        }
-
+        check_dgemm(a, b, d, c, n);
         // Wait for the child to finish and check the exit code.
         int child_status = 0;
         pid_t wait_pid = wait(&child_status);
